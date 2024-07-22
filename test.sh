6d8d726fe76eaaaa8bb1e481a38f4fe78e601a38
Szymon Gizler <sgizler@internships.antmicro.com>
2024-07-22 13:48:40 +0200

    Add prototype test.sh for Caliptra VCD bug

--- test.sh
+++ test.sh
@@ -1,24 +1,29 @@
 #!/bin/sh
 
-verilator --cc --autoflush --timescale 1ns/1ps --timing --top-module caliptra_top_tb "$1" \
-  -Wno-WIDTH -Wno-UNOPTFLAT -Wno-LITENDIAN -Wno-CMPCONST -Wno-MULTIDRIVEN -Wno-UNPACKED -Wno-ALWCOMBORDER 2>actual_stderr 
+verilator --cc -CFLAGS "-std=c++11" "$1" \
+  +libext+.v+.sv +define+RV_OPENSOURCE \
+  --timescale 1ns/1ps \
+  -I/home/ant/tmp/caliptra_workspace/caliptra-rtl/tools/scripts \
+  -Wno-WIDTH -Wno-UNOPTFLAT -Wno-LITENDIAN -Wno-CMPCONST -Wno-MULTIDRIVEN -Wno-UNPACKED -Wno-IMPLICITSTATIC \
+  --top-module caliptra_top_tb \
+   \
+  -exe test_caliptra_top_tb.cpp --autoflush --trace --trace-structs || exit 1
+#+define+CALIPTRA_INTERNAL_TRNG
+cp /home/ant/tmp/caliptra_workspace//caliptra-rtl//src/integration/tb/test_caliptra_top_tb.cpp obj_dir/
+make -j -e -C obj_dir/ -f Vcaliptra_top_tb.mk OPT_FAST="-O0" OPT_GLOBAL="-O0" VM_PARALLEL_BUILDS=1 2>/dev/null
 
-# if [ $? -ne 0 ]; then
-#   EXIT_CODE=$?
-# elif; then
-#   cmp actual_stderr golden_stderr
-#   EXIT_CODE=$?
-# fi
-sed 's/[a-z_-]*.sv//g' actual_stderr -i
-sed 's/:[0-9]*//g' actual_stderr -i
-sed 's/[0-9]* |//g' actual_stderr -i
-sed 's/[\t\n ]\+/ /g' actual_stderr -i
-printf "\n\n\n"
-diff golden_stderr actual_stderr --color && printf "SUCCESS\n\n\n"
+cleanup() {
+  jobs -p | xargs -r kill -9 # kill background jobs
+  wait
+}
+
+rm -f sim.vcd
+mkfifo sim.vcd
+trap cleanup EXIT INT HUP TERM
+timeout 3 ./obj_dir/Vcaliptra_top_tb &
+timeout 3 gawk '/var wire .* timer1_timeout_period\[/ {ID_TO_NAME[$4]=$5} {for(id in ID_TO_NAME) if($0 ~ id"$") print $1, ID_TO_NAME[id] } /^#/ && gensub("^#", "", "g")+0 >= 40{exit}' sim.vcd > actual_out || exit 1
+cleanup
+diff golden_out actual_out --color=always && printf "SUCCESS\n\n\n"
 EXIT_CODE=$?
-# cat actual_stderr
-
-[ -n "$GOLDEN" ] && cp actual_stderr golden_stderr
-
+[ -n "$GOLDEN" ] && cp actual_out golden_out
 exit "$EXIT_CODE"
-
