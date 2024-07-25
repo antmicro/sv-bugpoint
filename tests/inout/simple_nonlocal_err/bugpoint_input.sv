c64ec5135fcb671c86eab16559dba25c5a0c6683
Szymon Gizler <sgizler@internships.antmicro.com>
2024-07-25 17:10:12 +0200

    Add basic tests

---
+++ tests/inout/simple_nonlocal_err/bugpoint_input.sv
@@ -0,0 +1,63 @@
+// nonsense code that should error out on access to undefined struct member
+
+module full_adder (
+        input a,
+        input b,
+        input cin,
+        output s,
+        output cout);
+    assign s = a^b^cin;
+    assign cout = (a&b) | (b&cin) | (a&cin);
+endmodule
+
+module full_adder2 (
+        input a,
+        input b,
+        input cin,
+        output s,
+        output cout);
+    assign s = a^b^cin;
+    assign cout = (a&b) | (b&cin) | (a&cin);
+endmodule
+
+module full_adder3 (
+        input a,
+        input b,
+        input cin,
+        output s,
+        output cout);
+    assign s = a^b^cin;
+    assign cout = (a&b) | (b&cin) | (a&cin);
+endmodule
+
+typedef struct {
+        byte a;
+        int b;
+} struct_foo;
+
+module serial_adder #(WIDTH=32) (
+        input  [WIDTH-1:0] a,
+        input  [WIDTH-1:0] b,
+        input  cin,
+        output [WIDTH-1:0] s,
+        output cout);
+
+    wire [WIDTH:0] k;
+    wire [WIDTH:0] l;
+    wire [WIDTH:0] c;
+    wire [WIDTH:0] m;
+    wire [WIDTH:0] n = 4;
+
+    generate for (genvar i = 0; i < WIDTH; i++)
+        full_adder fa(a[i], b[i], c[i], s[i], c[i+1]);
+    endgenerate
+
+    struct_foo foo = '{5,6};
+    assign foo.a = 25;
+    assign foo.c = 0;
+    
+    assign c[0] = cin;
+    assign cout = c[WIDTH];
+
+endmodule
+
