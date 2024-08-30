ea7c25ea34a8017e5023cc2233a6d6f0a591850b
Szymon Gizler <sgizler@internships.antmicro.com>
2024-08-30 14:47:54 +0200

    Extract volatile metadata strip to separate script

--- examples/caliptra_verilation_err/bugpoint_check.sh
+++ examples/caliptra_verilation_err/bugpoint_check.sh
@@ -5,13 +5,8 @@
 # meant to debug case with caliptra-rtl@67fc658 and verilator@9a8e68928
 
 verilator --cc --autoflush --timescale 1ns/1ps --timing --top-module caliptra_top_tb "$1" \
-  -Wno-WIDTH -Wno-UNOPTFLAT -Wno-LITENDIAN -Wno-CMPCONST -Wno-MULTIDRIVEN -Wno-UNPACKED -Wno-ALWCOMBORDER 2>actual_stderr 
-
-# strip volatile metadata like filenames, filelines and redundant whitespace
-sed 's/[a-z_-]*.sv//g' actual_stderr -i
-sed 's/:[0-9]*//g' actual_stderr -i
-sed 's/[0-9]* |//g' actual_stderr -i
-sed 's/[\t\n ]\+/ /g' actual_stderr -i
+  -Wno-WIDTH -Wno-UNOPTFLAT -Wno-LITENDIAN -Wno-CMPCONST -Wno-MULTIDRIVEN -Wno-UNPACKED -Wno-ALWCOMBORDER \
+  2>&1 >/dev/null | bugpoint_strip_verilator_errmsg > actual_stderr
 
 printf "\n\n\n"
 diff golden_stderr actual_stderr --color && printf "SUCCESS\n\n\n"
