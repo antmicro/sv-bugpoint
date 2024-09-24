900c1c4202a4e54fe7f2bb5d3e48d27fc7679645
Szymon Gizler <sgizler@internships.antmicro.com>
2024-09-24 19:22:56 +0200

    Add shellcheck linter

--- check.mk
+++ check.mk
@@ -7,6 +7,9 @@ test_and_lint_dont_abort_after_err:
 .PHONY: test_and_lint
 test_and_lint: test lint
 
+.PHONY: autofix
+autofix: autofmt shellcheck_autofix
+
 .PHONY: autofmt
 autofmt:
 	clang-format-14 -i source/*.cpp source/*.hpp
@@ -16,7 +19,7 @@ test:
 	@$(MAKE) -C tests
 
 .PHONY: lint
-lint: fmt_check
+lint: fmt_check shellcheck
 
 .PHONY: fmt_check
 fmt_check:
@@ -24,3 +27,17 @@ fmt_check:
 	  diff "$$i" <(clang-format-14 "$$i")  --label "original $$i" --label "formatted $$i" --color=always -u; \
 	done
 	@echo # blank line for consistency
+
+
+SCRIPTS=scripts/* tests/*.sh tests/run_test \
+	examples/caliptra_vcd/sv-bugpoint-check.sh \
+	examples/caliptra_verilation_err/sv-bugpoint-check.sh
+
+.PHONY: shellcheck
+shellcheck:
+	shellcheck $(SCRIPTS) --color
+
+.PHONY: shellcheck
+shellcheck_autofix:
+	shellcheck $(SCRIPTS) -f diff --color || exit 0
+	shellcheck $(SCRIPTS) -f diff | git apply
