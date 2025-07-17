7aa4ff5fb004606c86e04267a79b1908b9676633
Szymon Gizler <sgizler@antmicro.com>
2025-07-17 12:53:02 +0200

    Update caliptra_verilation_err example

---
+++ examples/caliptra_verilation_err/out/sv-bugpoint-combined.sv
@@ -0,0 +1,32 @@
+
+`begin_keywords "1800-2023"
+module spiflash #(
+) (
+  input logic sck
+);
+  task automatic return_byte( int data, int io_mode);
+    automatic int unsigned rails;
+    repeat(8/rails) begin
+      fork
+        begin
+          @(negedge sck);
+        end
+      join_any
+    end
+  endtask : return_byte
+endmodule: spiflash
+`begin_keywords "1800-2023"
+`default_nettype none
+module caliptra_top_tb (
+    );
+    logic                                qspi_clk;
+spiflash spiflash0 (
+  .sck (qspi_clk)
+);
+endmodule
+`begin_keywords "1800-2023"
+`begin_keywords "1800-2023"
+`begin_keywords "1800-2023"
+`begin_keywords "1800-2023"
+`begin_keywords "1800-2023"
+`begin_keywords "1800-2023"
